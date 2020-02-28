76d503358e15f260bd8dc5cc78ff691d3456339f
Callum Quinn <Callum_Quinn1@outlook.com>
2020-02-28 11:52:14 +0000

    Added 360 degree Attacking. With help from Thomas O'Leary.

--- Assets/RotateFirePoint.cs
+++ Assets/RotateFirePoint.cs
@@ -6,14 +6,38 @@ public class RotateFirePoint : MonoBehaviour
 {
 
     public Camera cam;
-    public Rigidbody2D rotateFirePointrb;
+    // public Rigidbody2D rotateFirePointrb;
+    public Transform firePointRotate;
     Vector2 mousePos;
 
-    void Update()
+    [SerializeField] 
+    private float radius;
+    private Transform pivot;
+
+    void Start()
     {
-        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 lookDir = mousePos - rotateFirePointrb.position;
-        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
-        rotateFirePointrb.rotation = angle;
+        pivot = firePointRotate.transform;
+        transform.parent = pivot;
+        transform.position += Vector3.up * radius;
+    }
+
+    void FixedUpdate()
+    {
+
+        //mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        //Vector2 lookDir = mousePos - rotateFirePointrb.position;
+
+        //float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        //rotateFirePointrb.rotation = angle;
+
+        Vector3 fpVector = Camera.main.WorldToScreenPoint(firePointRotate.position);
+        fpVector = Input.mousePosition - fpVector;
+        float angle = Mathf.Atan2(fpVector.y, fpVector.x) * Mathf.Rad2Deg;
+
+        pivot.position = firePointRotate.position;
+        pivot.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+
+
     }
 }
